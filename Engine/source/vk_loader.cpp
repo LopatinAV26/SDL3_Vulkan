194a91ed4a703b384160bf102f8714311bf06257
Lopatin AV (Linux) <LopatinAV26@gmai.com>
2025-02-05 18:48:40 +0900

    fastgltf trouble

--- Engine/source/vk_loader.cpp
+++ Engine/source/vk_loader.cpp
@@ -15,6 +15,7 @@
 
 std::optional<std::vector<std::shared_ptr<MeshAsset>>> loadGltfMeshes(VulkanEngine* engine, std::filesystem::path filePath)
 {
+	//open mesh------------
 	std::cout << "Loading GLTF: " << filePath << std::endl;
 
 	fastgltf::GltfDataBuffer data;
@@ -32,5 +33,49 @@ std::optional<std::vector<std::shared_ptr<MeshAsset>>> loadGltfMeshes(VulkanEngi
 	else {
 		std::print("Failed to load glTF: {} \n", fastgltf::to_underlying(load.error()));
 		return{};
+	}
+	//open mesh--------------------
+
+	//load mesh---------------------
+	std::vector<std::shared_ptr<MeshAsset>> meshes;
+
+	//use the same vectors for all meshes so that the memory doesnt reallocate as often
+	std::vector<uint32_t> indices;
+	std::vector<Vertex> vertices;
+	for(fastgltf::Mesh& mesh: gltf.meshes){
+		MeshAsset newMesh;
+		newMesh.name = mesh.name;
+
+		//clear the mesh arrays each mesh, we dont want to merge them by error
+		indices.clear();
+		vertices.clear();
+
+		for(auto&& p: mesh.primitives){
+			GeoSurface newSurface;
+			newSurface.startIndex = static_cast<uint32_t>(indices.size());
+			newSurface.count = static_cast<uint32_t>(gltf.accessors[p.indicesAccessor.value()].count);
+
+			size_t initial_vtx = vertices.size();
+
+			//load indexes
+			{
+				fastgltf::Accessor& indexaccessor = gltf.accessors[p.indicesAccessor.value()];
+				indices.reserve(indices.size() + indexaccessor.count);
+
+				fastgltf::iterateAccessor<std::uint32_t>(gltf, indexaccessor, [&](std::uint32_t idx){
+					indices.push_back(idx + initial_vtx);
+				});
+			}
+
+			//load vertex positions
+			{
+				fastgltf::Accessor& posAccessor = gltf.accessors[p.findAttribute("POSITION")->accessorIndex];
+				vertices.resize(vertices.size() + posAccessor.count);
+
+				fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, posAccessor, [&](glm::vec3 v, size_t index){
+					Vertex newVtx;
+				});
+			}
+		}
 	}
 }
